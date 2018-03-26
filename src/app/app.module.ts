3c4f018d18742d6193bf7a0a29a6e12e4a854615
mugala <mugalalevi21@gmail.com>
2018-03-26 15:23:31 +0300

    Add Component and service folder This commit will add the component and the service folder to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GitsearchComponent } from './components/gitsearch/gitsearch.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitsearchComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/services/gitsearch.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GitsearchService {
+  private username:string;
+
+  //Use client id and client secret whilst we fetch data from github api to avoid limit restriction.
+  private clientid = '865f88da5f9ccdcb077a';
+  private clientsecret = '36bb92a36bc9c90b76cbd872bdb30394e7f4f4e6';
+
+  constructor(private http:Http) { 
+    console.log("service works");
+    this.username = 'Mugala'; 
+
+  }
+  getProfileDesc(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + );
+  }
+
+}
